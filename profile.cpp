911b7ea71c97d1009ab7f3acd53f95d127356939
NilotpalChoudhury <nilotpalchoudhury13@gmail.com>
2026-08-16 01:55:08 +0530

    Print name and roll number in profile

--- profile.cpp
+++ profile.cpp
@@ -4,4 +4,6 @@ using namespace std;
 
 void printProfile(string name, int rollNumber) {
     cout << "Profile details:" << endl;
+    cout << "Name: "<<name<<endl;
+    cout<<Roll Number: "<< rollNumber<<endl;
 }
