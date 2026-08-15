d96904d8d4c219098595ef9e5866236274b62fd3
NilotpalChoudhury <nilotpalchoudhury13@gmail.com>
2026-08-16 00:31:00 +0530

    Add basic login function without validation

---
+++ login.cpp
@@ -0,0 +1,8 @@
+// login.cpp
+#include "login.h"
+#include <iostream>
+using namespace std;
+
+void login(string username, string password) {
+    cout << "Login successful for user: " << username << endl;
+}
