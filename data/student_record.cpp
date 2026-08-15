60cdc4eace925627a5992ea1fe4dc4986a4fb85e
NilotpalChoudhury <nilotpalchoudhury13@gmail.com>
2026-08-15 23:51:07 +0530

    Restore student info file from repository history

---
+++ data/student_record.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+
+void displayStudentInfo(const std::string& name, const std::string& rollNumber) {
+    std::cout << "Student Name: " << name << std::endl;
+    std::cout << "Roll Number: " << rollNumber << std::endl;
+}
