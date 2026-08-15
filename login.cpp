0429bf5d763d9e837eb83daa5aed24418ca87ca8
NilotpalChoudhury <nilotpalchoudhury13@gmail.com>
2026-08-16 00:33:13 +0530

    Integrate existing username validation into login

--- login.cpp
+++ login.cpp
@@ -1,8 +1,11 @@
-// login.cpp
 #include "login.h"
 #include <iostream>
 using namespace std;
 
 void login(string username, string password) {
+    if (!validateUsername(username)) {
+        cout << "Login failed: invalid username." << endl;
+        return;
+    }
     cout << "Login successful for user: " << username << endl;
 }
