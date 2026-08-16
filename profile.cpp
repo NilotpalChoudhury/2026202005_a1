6cb8349e039c7e0cad9489217c4502a21d5ee620
NilotpalChoudhury <nilotpalchoudhury13@gmail.com>
2026-08-16 11:39:47 +0530

    Handle missing name case in profile display

--- profile.cpp
+++ profile.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 void printProfile(string name, int rollNumber) {
+   if (name.empty()) {
+        cout << "Profile unavailable: name not set." << endl;
+        return;
+    }
     cout << "Profile details:" << endl;
     cout << "Name: "<<name<<endl;
     cout<<Roll Number: "<< rollNumber<<endl;
