7dbfcd56ec12607e81c725430d3556d2dbc839de
NilotpalChoudhury <nilotpalchoudhury13@gmail.com>
2026-08-16 12:23:17 +0530

    Implement language change in settings module

---
+++ settings.cpp
@@ -0,0 +1,9 @@
+// settings.cpp
+#include "settings.h"
+#include <iostream>
+using namespace std;
+
+ void changeLanguage(string language) {
+    cout << "Language changed to: " << language << endl;
+}
+
