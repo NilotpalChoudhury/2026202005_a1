81622a25e8e503e73b6e6d0e4598fbbb864b4f52
NilotpalChoudhury <nilotpalchoudhury13@gmail.com>
2026-08-16 12:06:41 +0530

    Print and display average grade in dashboard output

--- dashboard.cpp
+++ dashboard.cpp
@@ -4,5 +4,7 @@ using namespace std;
 
 double averageGrade(double grade1, double grade2, double grade3) {
     double sum = grade1 + grade2 + grade3;
-    return sum;
+    double avg = sum/3;
+    cout << "Average grade : "<<avg<<endl;
+    return avg;
 }
