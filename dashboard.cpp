e0da3914cda71d80c163893ee2102dcad44f6ca1
NilotpalChoudhury <nilotpalchoudhury13@gmail.com>
2026-08-16 12:01:43 +0530

    Compute sum of grades in dashboard function module

---
+++ dashboard.cpp
@@ -0,0 +1,8 @@
+#include "dashboard.h"
+#include <iostream>
+using namespace std;
+
+double averageGrade(double grade1, double grade2, double grade3) {
+    double sum = grade1 + grade2 + grade3;
+    return sum;
+}
