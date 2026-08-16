db06be37a7a4c2d0a11308d30fd7749899c12d86
NilotpalChoudhury <nilotpalchoudhury13@gmail.com>
2026-08-16 12:09:24 +0530

    Add a grading system to dashboard

--- dashboard.cpp
+++ dashboard.cpp
@@ -6,5 +6,17 @@ double averageGrade(double grade1, double grade2, double grade3) {
     double sum = grade1 + grade2 + grade3;
     double avg = sum/3;
     cout << "Average grade : "<<avg<<endl;
+
+    if (avg >= 75) {
+        cout << "Grade: A" << endl;
+    }
+     else if (avg >= 50) {
+        cout << "Grade: B" << endl;
+    }
+     else if(avg>=40) {
+        cout << "Grade: C" << endl;
+    }
+    else cout<<"Grade F"<<endl;
+
     return avg;
 }
