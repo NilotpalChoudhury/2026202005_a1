c8f412540a808e6b9e8123ec0b6f5d80fb8fa939
NilotpalChoudhury <nilotpalchoudhury13@gmail.com>
2026-08-16 12:27:10 +0530

    Reject empty language input in settings

--- settings.cpp
+++ settings.cpp
@@ -1,9 +1,12 @@
-// settings.cpp
 #include "settings.h"
 #include <iostream>
 using namespace std;
 
  void changeLanguage(string language) {
     cout << "Language changed to: " << language << endl;
+    if (language.empty()) {
+        cout << "Invalid language: cannot be empty." << endl;
+        return;
+    }
 }
 
