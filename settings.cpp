5e841f5595b121363c3dbf2a8908831ccd98203e
NilotpalChoudhury <nilotpalchoudhury13@gmail.com>
2026-08-16 14:37:40 +0530

    Default to English when no language specified

--- settings.cpp
+++ settings.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
  void changeLanguage(string language) {
     if (language.empty()) {
-        cout << "Invalid language: cannot be empty." << endl;
+        cout << "No language specified, defaulting to English" << endl;
         return;
     }
     if (language.length() > 20) {
